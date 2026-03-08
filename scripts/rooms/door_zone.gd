86633b7a05737c41b2088d3dcff0841054c06511
HoTung <hoviettung2@gmail.com>
2026-03-08 21:25:01 +0700

    feat: add map outside collision and game ysort

--- scripts/rooms/door_zone.gd
+++ scripts/rooms/door_zone.gd
@@ -9,3 +9,18 @@ func _on_body_entered(body):
 func _on_body_exited(body):
 	if body.is_in_group("player"):
 		roof_layer.visible = true
+
+#extends Area2D
+#
+#@export var interior_root: Node2D
+#@export var exterior_root: Node2D
+#
+#func _on_body_entered(body):
+	#if body.is_in_group("player"):
+		#interior_root.visible = true
+		#exterior_root.visible = false
+#
+#func _on_body_exited(body):
+	#if body.is_in_group("player"):
+		#interior_root.visible = false
+		#exterior_root.visible = true
