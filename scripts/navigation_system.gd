1a230f1521b0ee2499a74b5866310b804b427f5a
HoTung <hoviettung2@gmail.com>
2026-03-19 19:45:18 +0700

    feat: update map_01

--- scripts/navigation_system.gd
+++ scripts/navigation_system.gd
@@ -1,59 +1,31 @@
 extends Node2D
 var waypoints = []
 var graph = {}
-
 var room_manager
+var MAX_DISTANCE = 200
 
-
-func _ready():
-	room_manager = get_parent().get_node("RoomManager")
-	scan_waypoints()
-	build_graph()
+func _ready() -> void:
+	pass
 func scan_waypoints():
-	var wp_container = get_parent().get_node("House/WayPoints_House")
-	waypoints = wp_container.get_children()
+	var wp_nodes = get_tree().get_nodes_in_group("waypoints")
+	waypoints = wp_nodes
+
 func build_graph():
 	for wp in waypoints:
-		graph[wp] = wp.neighbors
-func get_position_of(entity):
-	return entity.global_position
-#func get_my_position(entity):
-	#return entity.global_position
-	
-func get_nearest_waypoint(target_position):
-	var best_wp = null
-	var best_dist = INF
-	for wp in waypoints:
-		var d = target_position.distance_to(wp.global_position)
-		if d < best_dist:
-			best_dist = d
-			best_wp = wp
-	return best_wp
-	
-func get_neighbors_of(entity):
-	var wp = get_nearest_waypoint(entity.global_position)
-	return graph[wp]
-	
-func reconstruct_path(came_from, current):
-	var path = [current]
-	while came_from.has(current):
-		current = came_from[current]
-		path.push_front(current)
-	return path
-func find_path(start, goal):
-	var open = [start]
-	var came_from = {}
-	while open.size() > 0:
-		var current = open.pop_front()
-		if current == goal:
+		graph[wp] = []
+		for other in waypoints:
+			if wp == other:
+				continue
+			if can_connect(wp, other):
+				graph[wp].append(other)
 
-			return reconstruct_path(came_from, current)
-
-		for neighbor in graph[current]:
-
-			if not came_from.has(neighbor):
-
-				came_from[neighbor] = current
-
-				open.append(neighbor)
-	return []
+func can_connect(a, b):
+	if a.global_position.distance_to(b.global_position) > MAX_DISTANCE:
+		return false
+	var space = get_world_2d().direct_space_state
+	var query = PhysicsRayQueryParameters2D.create(
+		a.global_position,
+		b.global_position
+	)
+	var result = space.intersect_ray(query)
+	return result.is_empty()
